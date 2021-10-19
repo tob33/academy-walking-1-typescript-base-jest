4844109c2e49bf54732c2b14552ae9110ad05c4f
girikkoul <girik.koul@cazoo.co.uk>
2021-10-19 15:57:07 +0100

    Squashed commit of the following:
    commit eef77c968c8354a7997931c9c146a983b42ebabf
    Author: girikkoul <girik.koul@cazoo.co.uk>
    Date:   Tue Oct 19 15:56:08 2021 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 7190ebcd9971fa42b90d03fdb9bd2d18651dc7cd
    Author: Tobias Friedrich <tobias.friedrich@cazoo.co.uk>
    Date:   Tue Oct 19 16:20:10 2021 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 347b3bcfd63a48ac5654020f5dfe22298ded78d9
    Author: girikkoul <girik.koul@cazoo.co.uk>
    Date:   Tue Oct 19 15:07:08 2021 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 71d512f467e90899bd974291aa49a584c8851a4f
    Author: Tobias Friedrich <tobias.friedrich@cazoo.co.uk>
    Date:   Tue Oct 19 15:52:59 2021 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    # mob automatically added all co-authors from WIP commits
    # add missing co-authors manually
    Co-authored-by: Tobias Friedrich <tobias.friedrich@cazoo.co.uk>

--- src/main/example.ts
+++ src/main/example.ts
@@ -1,5 +1,22 @@
-export class Example {
-    add(a: number, b: number) {
-        return a + b;
+export class LeapYear {
+    isLeapYear(year: number) {
+        if (LeapYear.isAnAtypicalCommonYear(year))
+        {
+            return LeapYear.isAnAtypicalLeapYear(year);
+
+        }
+        return LeapYear.isALeapYear(year);
+    }
+
+    private static isALeapYear(year: number) {
+        return year % 4 === 0;
+    }
+
+    private static isAnAtypicalCommonYear(year: number) {
+        return year % 100 === 0;
+    }
+
+    private static isAnAtypicalLeapYear(year: number) {
+        return year % 400 === 0;
     }
 }
--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,8 +1,66 @@
-import {Example} from "../main/example";
+import {LeapYear} from "../main/example";
 
-describe('example test', () => {
-    it("should give sum of a + b when adding a and b", () => {
-        let example: Example = new Example();
-        expect(example.add(1, 2)).toBe(3);
+describe('whether a year is a leapYear', () => {
+    describe('For an atypical year which is divisible by 4 and 100, should always return false', () => {
+        it.each`
+        input | output | description
+        ${100} |${false} | ${"year 100 is not a leap year"}  
+        ${200} |${false} | ${"year 200 is not a leap year"}  
+        ${1900} |${false} | ${"year 1900 is not a leap year"}  
+        `(
+            "should output $output when input is $input $description",
+            ({ input, output}) => {
+
+                let leapYear: LeapYear = new LeapYear();
+                expect(leapYear.isLeapYear(input)).toBe(output);
+            }
+        );
+    })
+
+    describe('For a typical year which is not divisible by 4, should always return false', () => {
+        it.each`
+        input | output | description
+        ${15} |${false} | ${"year 15 is not a leap year"}  
+        ${17} |${false} | ${"year 17 is not a leap year"}  
+        ${2001} |${false} | ${"year 2001 is not a leap year"}  
+        `(
+            "should output $output when input is $input $description",
+            ({ input, output}) => {
+
+                let leapYear: LeapYear = new LeapYear();
+                expect(leapYear.isLeapYear(input)).toBe(output);
+            }
+        );
+    })
+
+    describe('For a typical leap year which is divisible by 4, should always return true', () => {
+        it.each`
+            input | output | description
+            ${16} |${true} | ${"year 16 is a leap year"}  
+            ${1996} |${true} | ${"year 1996 is a leap year"}  
+        `(
+            "should output $output when input is $input $description",
+            ({ input, output}) => {
+
+                let leapYear: LeapYear = new LeapYear();
+                expect(leapYear.isLeapYear(input)).toBe(output);
+            }
+        );
+    })
+
+    describe('For an atypical leap year which is divisible by 4 and 400, should always return true', () => {
+        it.each`
+            input | output | description
+            ${400} |${true} | ${"year 400 is a leap year"}  
+            ${1600} |${true} | ${"year 1600 is a leap year"} 
+            ${2000} |${true} | ${"year 2000 is a leap year"} 
+        `(
+            "should output $output when input is $input $description",
+            ({ input, output}) => {
+
+                let leapYear: LeapYear = new LeapYear();
+                expect(leapYear.isLeapYear(input)).toBe(output);
+            }
+        );
     })
 })
