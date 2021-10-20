f60ed3f505f0852048c360f1f2311fbfe2d29539
Tobias Friedrich <tobias.friedrich@cazoo.co.uk>
2021-10-20 12:00:56 +0200

    added more tests

--- src/main/fibonacci.ts
+++ src/main/fibonacci.ts
@@ -1,8 +1,9 @@
 export class Fibonacci {
-    getOutput(position: number) {
-        if (position === 1){
-            return 1;
-        }
-        return 0;
+    getOutput(position: number): number {
+    if (position === 0 || position === 1) {
+        return position}
+    if (position > 1 && position <= 4) {
+        return position-1;}
+    return this.getOutput(position-1)+this.getOutput(position-2)
     }
 }
