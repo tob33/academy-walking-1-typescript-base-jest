736316f0edd5bdba22451fe5a97c34b2eea910c7
girikkoul <girik.koul@cazoo.co.uk>
2022-01-27 16:27:12 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/example.ts
+++ src/main/example.ts
@@ -1,5 +1,32 @@
-export class Example {
-    add(a: number, b: number) {
-        return a + b;
+interface AccountService{
+    deposit(amount: number): void;
+     withdraw(amount: number): void;
+     printStatement() : void;
+}
+
+export interface IPrinter {
+    print(row:string[]):void
+}
+
+export class BankAccount implements AccountService {
+    printer:IPrinter;
+    private balance :number =0;
+    private amount :number =0;
+
+    constructor(printer:IPrinter) {
+        this.printer = printer
     }
+
+    deposit(amount: number): void{
+        this.balance = this.balance + amount;
+        this.amount = amount;
+    };
+
+    withdraw(amount: number): void{
+
+    };
+    printStatement() : void{
+        const statementLine = "10/01/2012" + " || " + this.amount + " || " + this.balance;
+        this.printer.print(["Date || Amount || Balance", statementLine])
+    };
 }
--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,8 +1,29 @@
-import {Example} from "../main/example";
+import {BankAccount, IPrinter} from "../main/example";
+import {mock} from 'jest-mock-extended'
 
-describe('example test', () => {
-    it("should give sum of a + b when adding a and b", () => {
-        let example: Example = new Example();
-        expect(example.add(1, 2)).toBe(3);
+describe('Bank Account', () => {
+    it("should return a header row only for an unused account", () => {
+        let printerMock = mock<IPrinter>();
+        let account = new BankAccount(printerMock)
+        account.printStatement()
+        expect(printerMock.print).toHaveBeenCalledWith(["Date || Amount || Balance"])
+    })
+
+    it("should update the balance when a deposit is made", () => {
+        let printerMock = mock<IPrinter>();
+        let account: BankAccount = new BankAccount(printerMock);
+        //spyOn(account, "printStatement").and.returnValues()
+        account.deposit(1000);
+        account.printStatement()
+        expect(printerMock.print).toHaveBeenCalledWith(["Date || Amount || Balance", "10/01/2012 || 1000 || 1000"])
+    })
+
+    it("should update the balance when a withdrawal is made", () => {
+        let printerMock = mock<IPrinter>();
+        let account: BankAccount = new BankAccount(printerMock);
+        //spyOn(account, "printStatement").and.returnValues()
+        account.withdraw(100);
+        account.printStatement()
+        expect(printerMock.print).toHaveBeenCalledWith(["Date || Amount || Balance", "10/01/2012 || 100 || -100"])
     })
 })
